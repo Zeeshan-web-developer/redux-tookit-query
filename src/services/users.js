7b515dd25779378e3cac3da2dc5863085a9fa766
Zeeshan Ahmad <zeeshana.codetown@gmail.com>
2022-08-10 10:35:18 +0530

    adding request headers

--- src/services/users.js
+++ src/services/users.js
@@ -1,17 +1,26 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-const baseUrl = "https://jsonplaceholder.typicode.com"
-
+const baseUrl = process.env.REACT_APP_BASE_API_URL
+console.log(baseUrl)
+const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7IklEIjoiNjJjNmUwYjRiYTE0MDhhMDI2ZTAwZjBkIiwidXNlclR5cGUiOiJCUkFORC1BRE1JTiJ9LCJpYXQiOjE2NTk5NzczNjB9.T335rv6G4O1xrjbctNHW2bUY7-zGnt5L3x2fFXEAlWk"
 export const quotesApi = createApi({
     reducerPath: "quotesApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: baseUrl,
+      baseUrl: baseUrl,
+      prepareHeaders: (headers, { getState }) => {
+            // const token = getState().users.token
+            // if (token) {
+          headers.set("Authorization", token)
+         headers.set("Content-Type", "application/json")
+            //}
+        return headers
+        }
   }),
 
     endpoints: (builder) => ({
         getUsers: builder.query({
       query: () => ({
-                url: `/users`,
-          method: "GET",
+                url: `/order/getAllPendingOrdersCount`,
+          method: "POST",
         responseHandler: (response) => response.json(),
       }),
     }),
